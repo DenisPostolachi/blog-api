6b23e7b3a50c770ca2480b5f8ec63f6f7a98a53f
DenisPostolachi <deny.salamanca@yandex.ru>
2022-10-24 16:49:09 +0300

    Sanctum Auth fixing

---
+++ app/Http/Requests/Auth/RegistrationRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Auth;
+
+use App\Http\Requests\BaseRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * @property string $name
+ * @property string $email
+ * @property string $password
+*/
+final class RegistrationRequest extends BaseRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+        ];
+    }
+}
+
---
+++ app/Http/Resources/Auth/RegistrationResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Auth;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RegistrationResource extends JsonResource
+{
+    private string $token;
+
+    public function __construct(string $token)
+    {
+        parent::__construct($token);
+
+        $this->token = $token;
+    }
+
+    public function toArray($request): array
+    {
+        return [
+            'access_token' => $this->token,
+            'token_type' => 'Bearer',
+        ];
+    }
+}
