22caf31958e5522ef0051fdb2b08a9fab03c6e9e
DenisPostolachi <deny.salamanca@yandex.ru>
2022-10-25 14:03:41 +0300

    Sanctum Auth fixing

--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -6,9 +6,7 @@ namespace App\Services\Auth;
 
 use App\Http\Requests\Auth\RegistrationRequest;
 use App\Models\User;
-use App\Repositories\Auth\AuthRepository;
 use Illuminate\Support\Facades\Hash;
-
 
 final class AuthService
 {
